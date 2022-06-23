437e65327b4756f6c0d4636554050468a37541b9
PixelPotato <a2da8@outlook.com>
2022-06-23 09:54:25 +0100

    added regex sample

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+#include <regex>
+#include <string>
 
-int main() {
-    std::cout << "Hello Parser" << std::endl;
+int main() {    
+    std::string str("Hello Regex, this is a test string");
+
+    // Whitespace Regex catch
+    std::regex wsReg("\\s+");
+
+    std::sregex_token_iterator iter(str.begin(),str.end(),wsReg, -1);
+    std::sregex_token_iterator end; // dummy iterator for vector below
+
+    std::vector<std::string> vec(iter, end);
+
+    for (auto a : vec)
+    {
+        std::cout << a << std::endl;
+    }
 }
